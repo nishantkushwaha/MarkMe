b336edab98d8851237910d18b6058e6f3af267c4
nishantkushwaha <nkush1998@gmail.com>
2018-12-01 16:06:33 +0530

    Initial commit

---
+++ app/src/main/java/com/rna/markme/Student/StudentLoginActivity.java
@@ -0,0 +1,74 @@
+package com.rna.markme.Student;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rna.markme.R;
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authstateListener;
+
+    private EditText emailText;
+    private EditText passText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+
+        emailText= (EditText) findViewById(R.id.userEmail);
+        passText= (EditText) findViewById(R.id.userPass);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        authstateListener= new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser()!=null){
+                    Intent intent = new Intent(StudentLoginActivity.this,StudentMainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authstateListener);
+    }
+
+    public void loginButtonClicked(View view) {
+        String email= emailText.getText().toString();
+        String pass =passText.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(this,"Enter both Values",Toast.LENGTH_LONG).show();
+        }
+
+        else{
+            mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(!task.isSuccessful()){
+                        Toast.makeText(StudentLoginActivity.this,"Incorrect Username or password",Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/rna/markme/Student/StudentMainActivity.java
@@ -0,0 +1,33 @@
+package com.rna.markme.Student;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.rna.markme.MainActivity;
+import com.rna.markme.R;
+
+public class StudentMainActivity extends AppCompatActivity {
+
+    TextView txt;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
+        txt=(TextView)findViewById(R.id.textView);
+        txt.setText(email.substring(0, email.length() - 10));
+    }
+
+    public void signOut(View view){
+        mAuth= FirebaseAuth.getInstance();
+        mAuth.signOut();
+        startActivity(new Intent(StudentMainActivity.this,StudentLoginActivity.class));
+    }
+}
